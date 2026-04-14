1e7549fe527485146a03faf09297d5e9d70b8cb9
SathwikPatel1 <sathwikpatel121@gmail.com>
2026-04-14 11:43:43 +0530

    utilization service

---
+++ src/pages/resource_management/services/utilizationService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const TMS_API_BASE_URL = window.__APP_CONFIG__?.TIMESHEET_API_ENDPOINT;
+
+export const getBillNonBillable = async (startDate, endDate) => {
+    try {
+        const response = await axios.get(`${TMS_API_BASE_URL}/api/users/hours`, {
+            params: {
+                startDate: startDate,
+                endDate: endDate,
+            },
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching bill non-billable:", error);
+        throw error;
+    }
+};
