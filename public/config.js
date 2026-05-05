1799f89401d5a8a0dc3aed639867ee1c2270047a
Sumiya Patan <sumiyap78@gmail.com>
2026-05-05 18:00:09 +0530

    rms integration

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 // public/config.js
 window.__APP_CONFIG__ = {
   TIMESHEET_API_ENDPOINT: "http://13.202.204.204:5000",
-  USER_MANAGEMENT_URL: "http://13.48.18.145",
+  USER_MANAGEMENT_URL: "http://13.204.95.26:8000",
   BASE_URL: "http://16.16.202.195:9999",
   PMS_BASE_URL: "http://13.127.14.175",
   MSOffice_USER_MANAGEMENT_URL: "http://13.48.18.145",
