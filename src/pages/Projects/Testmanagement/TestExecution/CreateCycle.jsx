1f080480464093fe44475d5bd827a0ef0042e905
vijay020 <vijayadurgabalda@gmail.com>
2026-05-15 15:43:48 +0530

    test runs

--- src/pages/Projects/Testmanagement/TestExecution/CreateCycle.jsx
+++ src/pages/Projects/Testmanagement/TestExecution/CreateCycle.jsx
@@ -104,13 +104,13 @@ export default function CreateTestCycleForm({
       setLoading(true);
       if (isEditMode) {
         await axiosInstance.put(
-          `/test-execution/test-cycles/${editingCycle.id}`, // ✅ correct
+          `/api/test-execution/test-cycles/${editingCycle.id}`, // ✅ correct
           payload
         );
         showStatusToast("Test Cycle Updated Successfully!", "success");
       } else {
         await axiosInstance.post(
-          `/test-execution/test-cycles`, // ✅ correct
+          `/api/test-execution/test-cycles`, // ✅ correct
           payload
         );
         showStatusToast("Test Cycle Created Successfully!", "success");
