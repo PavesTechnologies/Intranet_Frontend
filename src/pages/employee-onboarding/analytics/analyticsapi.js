e66bae46733acbd284845a45978b1c7b08c46a9f
Sindhu <sindhuredde07@gmail.com>
2026-06-03 12:49:56 +0530

    Update fetchDashboardAnalytics to use api.get
    Refactor fetchDashboardAnalytics to use api.get instead of fetch.

--- src/pages/employee-onboarding/analytics/analyticsapi.js
+++ src/pages/employee-onboarding/analytics/analyticsapi.js
@@ -3,16 +3,15 @@ import api from "../../../api/axiosInstance";
 export const fetchDashboardAnalytics = async () => {
   try {
     const token = localStorage.getItem("token"); // or your auth store
-
-    const response = await fetch(
-      `${import.meta.env.VITE_EMPLOYEE_ONBOARDING_URL}/analytics/dashboard`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      },
-    );
+    const response = await api.get(
+          `${window.__APP_CONFIG__.EMPLOYEE_ONBOARDING_URL}/analytics/dashboard`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          },
+        );
     return response.data;
   } catch (error) {
     console.error("Analytics API Error:", error);
