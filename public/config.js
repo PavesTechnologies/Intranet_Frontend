75aa46ddf122cf28126b52a20295535804489e3b
Alwalaswarnaraj <allwalaswarnaraj541@gmail.com>
2026-06-11 13:50:02 +0530

    changes in the hr tools add the approval rules button for the super admin and admin to access the approval rules page

--- public/config.js
+++ public/config.js
@@ -3,6 +3,7 @@ window.__APP_CONFIG__ = {
     TIMESHEET_API_ENDPOINT: "https://d15j2ej3bear0q.cloudfront.net/tms",
     USER_MANAGEMENT_URL: "https://d15j2ej3bear0q.cloudfront.net/ums",
     BASE_URL: "https://d15j2ej3bear0q.cloudfront.net/lms",
+    // BASE_URL: "http://localhost:9999/lms",
     PMS_BASE_URL: "https://d15j2ej3bear0q.cloudfront.net/pms",
     MSOffice_USER_MANAGEMENT_URL: "https://d15j2ej3bear0q.cloudfront.net/ums",
     EMPLOYEE_ONBOARDING_URL: "https://d15j2ej3bear0q.cloudfront.net/ems",
