c9833bf72fa1ee697075560e67282c037b6a7a85
SathwikPatel1 <sathwikpatel121@gmail.com>
2026-07-02 15:33:22 +0530

    airs dashboard & jd

--- public/config.js
+++ public/config.js
@@ -8,6 +8,7 @@ window.__APP_CONFIG__ = {
     MSOffice_USER_MANAGEMENT_URL: "https://d15j2ej3bear0q.cloudfront.net/ums",
     // EMPLOYEE_ONBOARDING_URL: "http://localhost:8001/ems",
     EMPLOYEE_ONBOARDING_URL: "https://d15j2ej3bear0q.cloudfront.net/ems",
-    RMS_BASE_URL: "https://d15j2ej3bear0q.cloudfront.net/rms"
+    RMS_BASE_URL: "https://d15j2ej3bear0q.cloudfront.net/rms",
+    AIRS_BASE_URL : "http://127.0.0.1:8000",
     //RMS_BASE_URL: "http://localhost:8080/rms"
 };
