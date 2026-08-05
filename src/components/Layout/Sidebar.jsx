184017d72ceb10de17204c354a86b0220acfa27f
Sumiya Patan <sumiyap78@gmail.com>
2026-08-05 11:35:34 +0530

    fix: simplify condition for rendering child menu in Sidebar component

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -578,7 +578,7 @@ const Sidebar = ({ isCollapsed }) => {
                 )}
               </div>
 
-              {xmsHovered && childMenuOwner === "eo" && (
+              {xmsHovered && (
                 <ul
                   className={`fixed w-fit min-w-[220px] whitespace-nowrap bg-white text-[#0a174e] rounded-lg shadow-2xl z-[9999] py-2 border ${isCollapsed ? "left-20" : "left-64"
                     }`}
